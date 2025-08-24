105f76807e1ec3fb51cae0905c7267b1d94a1243
DiegoTrader2 <diegokgomez2@gmail.com>
2025-08-24 17:39:21 -0300

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
-# Poner el token y chat_id directo para probar
-TELEGRAM_BOT_TOKEN = "TELEGRAM_TOKEN"
-CHAT_ID = "TELEGRAM_CHAT_ID"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
@@ -16,25 +16,27 @@ def send_telegram_message(message):
 def alert():
     data = request.json
 
-    signal = data.get("signal", "").lower()
+    signal = data.get("signal", "alerta")
     symbol = data.get("symbol", "N/A")
     price = data.get("price", "N/A")
     interval = data.get("interval", "N/A")
     time = data.get("time", "N/A")
 
-    # Convertimos señal
-    if signal == "buy":
-        action = "Posible COMPRA"
-    elif signal == "sell":
-        action = "Posible VENTA"
+    # Convertimos señal en texto claro
+    if signal.lower() == "buy":
+        action = "📈 Posible COMPRA"
+    elif signal.lower() == "sell":
+        action = "📉 Posible VENTA"
     else:
-        action = "⚠️ Señal desconocida"
+        action = "⚠ Señal detectada"
 
-    message = f"""Par: {symbol}
-Señal: {action}
+    message = f"""
+{action}
+Par: {symbol}
 Precio: {price}
 Temporalidad: {interval}
-Hora: {time}"""
+Hora: {time}
+"""
 
     send_telegram_message(message)
     return "ok", 200
