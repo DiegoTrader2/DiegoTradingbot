adedec13f5585ffa1accee487b9d38a1a2504978
DiegoTrader2 <diegokgomez2@gmail.com>
2025-08-24 03:54:32 -0300

    app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import requests
 app = Flask(__name__)
 
 # Poner el token y chat_id directo para probar
-TELEGRAM_BOT_TOKEN = "TU_TOKEN_AQUI"
-CHAT_ID = "TU_CHAT_ID_AQUI"
+TELEGRAM_BOT_TOKEN = "TELEGRAM_TOKEN"
+CHAT_ID = "TELEGRAM_CHAT_ID"
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
